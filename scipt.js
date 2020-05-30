8c3200d1e75e73fc1bf57f820f7e150851185f52
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-05-30 18:12:38 -0400

    Created sample const for questions, will write questions tomorrow

--- scipt.js
+++ scipt.js
@@ -1,3 +1,25 @@
-const question = document.getElementById("question");
-const answer = document.getElementById("answer");
-const start = document.getElementById("start");
+const questionEl = document.getElementById("question");
+const answerEl = document.getElementById("answer");
+const startEl = document.getElementById("start");
+const quizQuestions = [
+  {
+    question: "INSERT QUESTION HERE?",
+    answer: {
+      1: "INSERT ANSWER HERE",
+      2: "INSERT ANSWER HERE",
+      3: "INSERT ANSWER HERE",
+      4: "INSERT ANSWER HERE",
+    },
+    correctAnswer: "INSERT 1-4",
+  },
+  {
+    question: "INSERT QUESTION HERE?",
+    answer: {
+      1: "INSERT ANSWER HERE",
+      2: "INSERT ANSWER HERE",
+      3: "INSERT ANSWER HERE",
+      4: "INSERT ANSWER HERE",
+    },
+    correctAnswer: "INSERT 1-4",
+  },
+];
