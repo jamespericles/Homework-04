887ebc22a9da5792d9ce9f0c4f92146dedc84e3a
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-05-31 17:56:09 -0400

    Updated ids for questions. Added questions. Updated description of quiz. Add constants for questions

--- scipt.js
+++ scipt.js
@@ -1,33 +1,67 @@
+// Variables
 const questionEl = document.getElementById("question");
 const answerEl = document.getElementById("answer");
-const startEl = document.getElementById("start");
+const startEl = document.getElementsByClassName("start");
 const timerEl = document.getElementById("timer");
+const choice1 = document.getElementById("1");
+const choice2 = document.getElementById("2");
+const choice3 = document.getElementById("3");
+const choice4 = document.getElementById("4");
 let isWaiting = false;
 let isRunning = false;
 let seconds = 120;
 let finalCountdown = false;
-let countdownTimer;
 
 const quizQuestions = [
   {
-    question: "INSERT QUESTION HERE?",
+    question: "What is the symbol to comment in JavaScipt?",
     answer: {
-      1: "INSERT ANSWER HERE",
-      2: "INSERT ANSWER HERE",
-      3: "INSERT ANSWER HERE",
-      4: "INSERT ANSWER HERE",
+      choice1: "//",
+      choice2: "/* */",
+      choice3: "<-- -->",
+      choice4: "/*/*/",
     },
-    correctAnswer: "INSERT 1-4",
+    correctAnswer: "1",
   },
   {
-    question: "INSERT QUESTION HERE?",
+    question: "What does HTML stand for?",
     answer: {
-      1: "INSERT ANSWER HERE",
-      2: "INSERT ANSWER HERE",
-      3: "INSERT ANSWER HERE",
-      4: "INSERT ANSWER HERE",
+      choice1: "Hyper Text Meta Language",
+      choice2: "Hyper Time Meta Language",
+      choice3: "Hyper Text Markup Language",
+      choice4: "Language Markup Hyper Text",
     },
-    correctAnswer: "INSERT 1-4",
+    correctAnswer: "3",
+  },
+  {
+    question: "What file extension should CSS files have?",
+    answer: {
+      choice1: ".css",
+      choice2: ".cs",
+      choice3: ".csss",
+      choice4: ".cssc",
+    },
+    correctAnswer: "1",
+  },
+  {
+    question: "What is JavaScript meant to add to a webpage?",
+    answer: {
+      choice1: "Functionality",
+      choice2: "Stylings",
+      choice3: "Elements such as <div> and <body>",
+      choice4: "Words",
+    },
+    correctAnswer: "1",
+  },
+  {
+    question: "What is the root tag of HTML?",
+    answer: {
+      choice1: "<body>",
+      choice2: "<html>",
+      choice3: "<title>",
+      choice4: "<head>",
+    },
+    correctAnswer: "2",
   },
 ];
 
@@ -37,6 +71,7 @@ function gameTimer() {
   if (remainingSeconds < 10) {
     remainingSeconds = "0" + remainingSeconds;
   }
+
   document.getElementById("timer").innerHTML = minutes + ":" + remainingSeconds;
   if (finalCountdown) {
     clearInterval(countdownTimer);
@@ -45,4 +80,5 @@ function gameTimer() {
     seconds--;
   }
 }
+
 countdownTimer = setInterval(gameTimer, 1000);
